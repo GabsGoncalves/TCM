0ca2f518dd8ba596e338e2eb77911afc836a3569
gabriel-fodinha <127167783+gabriel-fodinha@users.noreply.github.com>
2023-09-08 19:56:00 -0300

    adição da amuletos funcional

---
+++ assets/js/amuletos.js
@@ -0,0 +1,6 @@
+function infosAmuleto(nomeAmuleto, cardInfo, cardInfo2, cardImagem) {
+    document.getElementById("nome_amuleto").innerHTML = nomeAmuleto;
+    document.getElementById("cardInfo").innerHTML = cardInfo;
+    document.getElementById("cardInfo2").innerHTML = cardInfo2;
+    document.getElementById("cardImagem").src = cardImagem;
+}
