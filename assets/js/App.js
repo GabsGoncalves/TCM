c877ea3c7999816522780231bd1a66426177bfcc
gabriel-fodinha <127167783+gabriel-fodinha@users.noreply.github.com>
2023-10-12 12:40:46 -0300

    terminei

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,10 +1,10 @@
 function menuShow() {
-    let menuMobile = document.querySelector('.new-mobile-menu'); // Substituído para .new-mobile-menu
+    let menuMobile = document.querySelector('.new-mobile-menu');
     if (menuMobile.classList.contains('new-open')) {
         menuMobile.classList.remove('new-open');
-        document.querySelector('.new-icon').src = "assets/img/menu_white_36dp.svg"; // Substituído para .new-icon
+        document.querySelector('.new-icon').src = "assets/img/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('new-open');
-        document.querySelector('.new-icon').src = "assets/img/close_white_36dp.svg"; // Substituído para .new-icon
+        document.querySelector('.new-icon').src = "assets/img/close_white_36dp.svg";
     }
 }
